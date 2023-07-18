326de00e8b37d50ebc5dae88c02d0950d29df39b
Harrisbaig7 <baigharris77@gmail.com>
2023-07-18 21:21:42 +0500

    keyframes Changes

---
+++ Introduction to JavasSript/JS Fundamentals/main.js
@@ -0,0 +1,31 @@
+console.log("Hello World!")
+console.log("Hello This Code is From my Html File");
+
+// PRIMITIVE DATA TYPE
+// type of data  can me  altered in js
+// does not provide any method all except string  data type
+// 7 types of primitive data types
+// String, Number, Big Int, Boolean, Null, Undefined, Symbol
+// String is a data type which is a collection of characters or sequence of characters
+// Big Int 2^53 -1 (huge number)
+// every value less than 2^53 -1 is a number
+// undefined does not have any value or data type also
+// Null is a data type which has no value
+// Boolean is true or false
+// any Number other than 0 is always true
+
+// NON PRIMITIVE DATA TYPE
+// Two kinds of non-primitive DATA: Objects and arrays
+
+// var name2 = "Harris";
+// var year = 2021;
+// console.log(name2 + " " + year);
+// // var name = "ahmed";
+// // var year = 2020;
+// // console.log(name + " " + year);
+// const name1 = "Harris";
+// const year = 2021;
+// console.log(name1 + " " + year);
+// const name = "Ahmed";
+// const year = 2021;
+// console.log(name + " " + year);
